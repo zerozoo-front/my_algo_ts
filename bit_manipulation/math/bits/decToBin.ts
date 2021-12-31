c282cc7e38570cd82c7d5f75d0ed0aae7d501258
윤영주 <user@yun-yeongjuui-MacBookAir.local>
2021-12-31 17:13:37 +0900

    ff

--- bit_manipulation/math/bits/decToBin.ts
+++
@@ -1,4 +0,0 @@
-function decToBin(dec: number) {
-  return (dec >>> 0).toString(2);
-}
-export default decToBin;
--- bit_manipulation/math/bits/readLine.ts
+++
@@ -1,6 +0,0 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-export default readline;
